66fb247ab45c19b1f46c2c2992b877056d2837e7
Mohammad Ishaq Khan <mohdishaqk2@gmail.com>
2025-09-28 18:24:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/notes", require("./routes/notes"));
 
 // Connect MongoDB
 mongoose.connect(process.env.MONGO_URI, {
@@ -18,6 +11,15 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log(err));
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
